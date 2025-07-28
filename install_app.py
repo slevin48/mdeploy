3ca7614bd4ced5a3e7ca9f8a9678fea6a2aac961
slevin48 <yann.debray@gadz.org>
2025-07-28 13:09:30 -0400

    Update app prefix

--- install_app.py
+++ install_app.py
@@ -2,7 +2,8 @@ import streamlit as st
 from matlab_runtime import install, guess_prefix
 
 version = "R2024b"
-install(version, auto_answer=True)
+prefix = "/usr/local/MATLAB/MATLAB_Runtime/" + version
+install(version, prefix=prefix, auto_answer=True)
 
 print(guess_prefix())
 st.write(guess_prefix())
