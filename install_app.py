2cd61b47959d04d253b65a0f7ed56a9258eb7efe
slevin48 <yann.debray@gadz.org>
2025-07-28 12:57:35 -0400

    Install matlab runtime to workspaceS

--- install_app.py
+++ install_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from matlab_runtime import install, guess_prefix
 
 version = "R2025a"
-install(version,prefix="/workspace/mdeploy", auto_answer=True)
+install(version, auto_answer=True)
 
 print(guess_prefix())
 st.write(guess_prefix())
